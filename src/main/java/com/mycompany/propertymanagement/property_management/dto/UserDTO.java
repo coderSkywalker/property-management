4db402cbbcb4b2df50c2af67e67ce8cb66c535ae
coderSkywalker <2marek.michalowski@gmail.com>
2024-10-09 19:54:52 +0200

    Adding user dto class

---
+++ src/main/java/com/mycompany/propertymanagement/property_management/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.mycompany.propertymanagement.property_management.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String ownerName;
+    private String ownerEmail;
+    private String phone;
+    private String password;
+}
