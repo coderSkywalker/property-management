2d8e42a123b33d83b4669b4ae0dbfb43e17a7191
coderSkywalker <2marek.michalowski@gmail.com>
2024-10-09 19:47:35 +0200

    Adding User entity and repository

--- src/main/java/com/mycompany/propertymanagement/property_management/entity/UserEntity.java
+++ src/main/java/com/mycompany/propertymanagement/property_management/entity/UserEntity.java
@@ -6,17 +6,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "PROPERTY_TABLE")
 @Getter
 @Setter
 @NoArgsConstructor
-public class PropertyEntity {
+@Table(name = "USER_TABLE")
+public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String title;
-    private String description;
-    private String address;
-    private Double price;
+    private String ownerName;
+    @Column(name = "EMAIL", nullable = false)
+    private String ownerEmail;
+    private String phone;
+    private String password;
 }
---
+++ src/main/java/com/mycompany/propertymanagement/property_management/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mycompany.propertymanagement.property_management.repository;
+
+import com.mycompany.propertymanagement.property_management.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
