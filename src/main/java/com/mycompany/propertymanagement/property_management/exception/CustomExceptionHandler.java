b0f3c094132680353beb6e4c2f2900176c10e0e5
coderSkywalker <2marek.michalowski@gmail.com>
2024-10-11 13:44:02 +0200

    Adding exception handler.

--- src/main/java/com/mycompany/propertymanagement/property_management/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/propertymanagement/property_management/exception/CustomExceptionHandler.java
@@ -2,16 +2,34 @@ package com.mycompany.propertymanagement.property_management.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException businessException) {
-        System.out.println("Business exception is thrown");
+
         return new ResponseEntity<>(businessException.getErrors(), HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldValidationException(MethodArgumentNotValidException manve) {
+
+        List<ErrorModel> errorModels = new ArrayList<>();
+        List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : fieldErrors) {
+            ErrorModel errorModel = new ErrorModel();
+            errorModel.setCode(fieldError.getCode());
+            errorModel.setMessage(fieldError.getDefaultMessage());
+            errorModels.add(errorModel);
+        }
+
+        return new ResponseEntity<>(errorModels, HttpStatus.BAD_REQUEST);
     }
 }
