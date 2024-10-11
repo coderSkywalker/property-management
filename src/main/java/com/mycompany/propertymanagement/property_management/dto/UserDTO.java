647083b2c3aacc488fc9929d0311757c31f19e78
coderSkywalker <2marek.michalowski@gmail.com>
2024-10-11 13:38:52 +0200

    Adding validation to dto.

--- src/main/java/com/mycompany/propertymanagement/property_management/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/property_management/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.mycompany.propertymanagement.property_management.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +16,12 @@ public class UserDTO {
 
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is mandatory")
+    @NotEmpty(message = "Owner Email can not be empty")
+    @Size(min = 5, max = 50, message = "Owner Email should be between 5 and 50 characters long")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password must be given")
+    @NotEmpty(message = "Password can not be empty")
     private String password;
 }
